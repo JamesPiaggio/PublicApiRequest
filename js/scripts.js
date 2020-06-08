e581a3c69cae9ec081f3b34cf55e520107529a8f
JamesPiaggio <james.piaggio13@gmail.com>
2020-06-08 16:31:10 -0400

    Displays 12 random users

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Global variables
-const randomGeneratorAPI = 'https://randomuser.me/api/';
+const randomGeneratorAPI = 'https://randomuser.me/api/?results=12';
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
 const body = document.querySelector('body');
@@ -8,8 +8,25 @@ const body = document.querySelector('body');
 async function getRandomUser (url) {
     const user = await fetch(url);
     const userJSON = await user.json();
-}
+    return userJSON;
+};
 
 async function generateProfile () {
-    
-}
+    const userList = await getRandomUser(randomGeneratorAPI);
+    let userHTML = '';
+    for (let i = 0; i < userList.results.length; i++) {
+        const currUser = userList.results[i];
+        userHTML += `<div class="card">
+                        <div class="card-img-container">
+                            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+                        </div>
+                        <div class="card-info-container">
+                            <h3 id="name" class="card-name cap">${currUser.name.first} ${currUser.name.last}</h3>
+                            <p class="card-text">${currUser.email}</p>
+                            <p class="card-text cap">${currUser.location.city}, ${currUser.location.state}</p>
+                        </div>
+                    </div><br>`;
+    };
+    gallery.innerHTML = userHTML;
+};
+console.log(generateProfile());
