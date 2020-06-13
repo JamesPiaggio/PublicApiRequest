04202b24be08c11a5758440e734015931cac7b50
JamesPiaggio <james.piaggio13@gmail.com>
2020-06-12 23:38:56 -0400

    Added functions to create cards and modals

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ async function getRandomUser (url) {
     // Returns data in JSON
     return userJSON;
 };
-console.log(getRandomUser(randomGeneratorAPI));
+
 // Function that appends searchbar
 function appendSearch () {
     searchContainer.innerHTML = `<form action="#" method="get">
@@ -22,27 +22,68 @@ function appendSearch () {
                                 </form>`;
 };
 
-// Function that create cards for each employee
-async function createCards () {
+// Function to create card with user info
+function appendCard (user, userHTML) {
+    // Template literal to add HTML
+    userHTML = `<div class="card">
+                    <div class="card-img-container">
+                        <img class="card-img" src="${user.picture.large}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                        <p class="card-text">${user.email}</p>
+                        <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+                    </div>
+                </div>`;
+    return userHTML;
+};
+
+// Function to create modal windows with user info
+function appendModal (user, modalContainer) {
+    modalContainer = `<div class="modal-container" hidden>
+                            <div class="modal">
+                                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                                <div class="modal-info-container">
+                                    <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+                                    <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                                    <p class="modal-text">${user.email}</p>
+                                    <p class="modal-text cap">${user.location.city}</p>
+                                    <hr>
+                                    <p class="modal-text">${user.phone}</p>
+                                    <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
+                                    <p class="modal-text">Birthday: 10/21/2015</p>
+                                </div>
+                            </div>
+                        </div>`;
+    return modalContainer;
+};
+
+// Function that creates page
+async function createPage () {
     appendSearch();
+    // Fetches data for users
     const userList = await getRandomUser(randomGeneratorAPI);
+    // Empty variables for Template literals
     let userHTML = '';
+    let modalContainer = '';
     // For loop to create each card
     for (let i = 0; i < userList.results.length; i++) {
         const currUser = userList.results[i];
-        // Template literal to add HTML
-        userHTML += `<div class="card">
-                        <div class="card-img-container">
-                            <img class="card-img" src="${currUser.picture.large}" alt="profile picture">
-                        </div>
-                        <div class="card-info-container">
-                            <h3 id="name" class="card-name cap">${currUser.name.first} ${currUser.name.last}</h3>
-                            <p class="card-text">${currUser.email}</p>
-                            <p class="card-text cap">${currUser.location.city}, ${currUser.location.state}</p>
-                        </div>
-                    </div><br>`;
+        gallery.innerHTML += appendCard(currUser, userHTML);
     };
-    // Adds userHTML to gallery div
-    gallery.innerHTML = userHTML;
+    // For loop to create modal for each user
+    for (let j = 0; j < userList.results.length; j++) {
+        const currUser = userList.results[j];
+        body.innerHTML += appendModal(currUser, modalContainer);
+    };
 };
-console.log(createCards());
+
+createPage();
+
+
+
+
+
+
+
+
